f453a57805db99b1968ba88e82bf25900e917036
Woody Zhou <wozhou@linkedin.com>
2019-12-07 21:25:56 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# spellingbee
+
+## Quick Start
+
+```
+# Test sequentially
+./spellingbee.sh A SEQ
+
+# Test randomly
+./spellingbee.sh A
+# OR
+./spellingbee.sh A RANDOM
+
+```
+
+Output
+
+```
+===== Start =====
+
+>> pieces <<
+Press [r] repeat, [s] repeat slowly, [d] definition or any key to continue ...s
+>> pieces <<
+Press [r] repeat, [s] repeat slowly, [d] definition or any key to continue ...
+
+>> pardon <<
+Press [r] repeat, [s] repeat slowly, [d] definition or any key to continue ...
+
+>> onshore <<
+Press [r] repeat, [s] repeat slowly, [d] definition or any key to continue ...r
+>> onshore <<
+Press [r] repeat, [s] repeat slowly, [d] definition or any key to continue ...^C
+```
---
+++ spellingbee.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+VOICE="Samantha"
+
+getc() {
+  IFS= read -r -n1 -p 'Press [r] repeat, [s] repeat slowly, [d] definition or any key to continue ...' "$@"
+}
+
+readword() {
+	if [ "$#" -gt 1 ] ; then
+		say -v "Samantha" -r $2 $1
+	else
+		say -v "Samantha" $1
+	fi
+}
+
+definition() {
+	open dict://$1
+}
+
+testword() {
+	WORD=$1
+	SPEED=$2
+	echo ""
+	echo ">> $WORD <<"
+	readword $WORD $SPEED
+	getc C
+	case $C in 
+		"r")
+			testword $WORD;;
+		"s")
+			testword $WORD 100;;
+		"d")
+			definition $WORD
+			testword $WORD
+			;;
+		*) ;;
+	esac
+}
+
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ] ; then
+  echo "Usage: $0 COLUMN_IDX [SEQ|RANDOM]" >&2
+  say -v $VOICE "Unexpected input parameters"
+  exit 1
+fi
+
+COLUMN=$1
+ORDER=${2:-RANDOM}
+
+if [ "$ORDER" != "SEQ" ] && [ "$ORDER" != "RANDOM" ] ; then
+  echo "Usage: $0 COLUMN_IDX [SEQ|RANDOM]" >&2
+  say -v $VOICE "Unexpected order type. Please use SEQ or RANDOM."
+  exit 1
+fi
+
+FILENAME="spelling_bee_$COLUMN.txt"
+
+if [ -f $FILENAME ]; then
+	COMMAND="cat $FILENAME"
+	if [ "$ORDER" = "RANDOM" ]; then 
+		COMMAND="cat $FILENAME | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);'"
+	fi
+	echo "===== Start ====="
+	say -v $VOICE "Start checking words of column '$COLUMN'"
+	say "..."
+	for WORD in `eval $COMMAND`
+	do
+		testword $WORD
+	done
+	echo "===== Finish ====="
+	say -v $VOICE "Column '$COLUMN' finished!"
+else
+  echo "$FILENAME not found... Exit!" >&2
+  say -v $VOICE "Cannot find word file of column '$COLUMN'. Please double check!"
+  exit 1
+fi
